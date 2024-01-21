24cb225ba956fef02ca1f4091104d6cf7459a7a3
Ivan Lashuk <85491523+Britva1910@users.noreply.github.com>
2024-01-21 19:42:39 +0200

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { writeFile } from "node:fs/promises";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const srcToFile = `${__dirname}/files/fresh.txt`;
+  const messageError = "FS operation failed";
+  const messageSuccess = "I am fresh and young";
+
+  try {
+    await writeFile(srcToFile, messageSuccess, { flag: "wx" });
+    console.log("File written successfully");
+  } catch (error) {
+    throw new Error(messageError);
+  }
 };
 
 await create();
