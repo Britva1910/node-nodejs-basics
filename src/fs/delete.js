7bc7aeaa5c0a78a1d714a10858f59c0912d46cdc
Ivan Lashuk <85491523+Britva1910@users.noreply.github.com>
2024-01-28 20:05:39 +0200

    feat: implement fs delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { rm } from "node:fs/promises";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  const src = `${__dirname}/files/fileToRemove.txt`;
+  const msgError = "FS operation failed";
+
+  try {
+    await rm(src);
+  } catch (err) {
+    throw new Error(msgError);
+  }
 };
 
 await remove();
