281c79782b1ed5f5be937c29016365de9e5f146d
Ivan Lashuk <85491523+Britva1910@users.noreply.github.com>
2024-01-28 20:46:28 +0200

    feat: implement zip compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
-const compress = async () => {
-    // Write your code here 
+import zlib from "zlib";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, "files/fileToCompress.txt");
+const archivePath = path.join(__dirname, "files/archive.gz");
+
+export const compress = async () => {
+  const gzip = zlib.createGzip();
+  const inp = fs.createReadStream(filePath);
+  const out = fs.createWriteStream(archivePath);
+
+  inp.pipe(gzip).pipe(out);
 };
 
-await compress();
+compress();
