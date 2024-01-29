880fd43655a02b3dfbe8b876d80485b990312781
Ivan Lashuk <85491523+Britva1910@users.noreply.github.com>
2024-01-29 18:55:41 +0200

    feat: stream transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform, pipeline } from "stream";
+
+const readable = process.stdin;
+const writable = process.stdout;
+
 const transform = async () => {
-    // Write your code here 
+  const transform = new Transform({
+    transform(chank, encoding, callback) {
+      const chankStringifies = chank.toString().trim();
+      const reverseChunk = chankStringifies.split("").reverse().join("");
+      callback(null, reverseChunk + "\n");
+    },
+  });
+  pipeline(readable, transform, writable, (err) => console.log(err));
 };
 
 await transform();
