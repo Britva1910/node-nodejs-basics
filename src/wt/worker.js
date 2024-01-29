e0efd87452474edd8898b5cf0b4b7c99a09b92ca
Ivan Lashuk <85491523+Britva1910@users.noreply.github.com>
2024-01-29 18:24:03 +0200

    feat: implement worker

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,17 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { workerData, parentPort } from "worker_threads";
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+export const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+export const sendResult = () => {
+  try {
+    parentPort.postMessage({
+      status: "resolved",
+      data: nthFibonacci(workerData),
+    });
+  } catch (err) {
+    parentPort.postMessage({ status: "resolved", data: null });
+  }
 };
 
 sendResult();
